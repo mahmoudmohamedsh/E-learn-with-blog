b5e52fa3e9f1ff0a741795f07517126fbb3f2e18
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-01-13 00:16:28 +0200

    test

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 //database config
 const DB_USER = process.env.MONGO_DB_USERNAME
 const DB_PASS = process.env.MONGO_DB_PWD
-// when starting app locally, use "mongodb://admin:password@localhost:27017" URL instead
-// let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASS}@mongodb`;
 
-let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASS}@localhost:27017`;
+let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASS}@mongodb`;
+// when starting app locally, use "mongodb://admin:password@localhost:27017" URL instead
+// let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASS}@localhost:27017`;
 
 //Middleware
 app.use(express.json());
